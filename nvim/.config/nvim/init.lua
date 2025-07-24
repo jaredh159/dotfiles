7af2e6b00e63fa81cb7262ea73defeb4db030cc4
Jared Henderson <jaredh159@users.noreply.github.com>
2025-07-24 12:06:22 -0400

    nvim: some lsp tweaks

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -549,27 +549,11 @@ require("lazy").setup({
             diagnostics = { disabled = { "macro-error" } },
           },
         },
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
-        --
-
+        jsonls = {},
+        zls = {}, -- zig
+        ts_ls = {}, -- typescript, but consider: https://github.com/pmizio/typescript-tools.nvim
         lua_ls = {
-          -- cmd = { ... },
-          -- filetypes = { ... },
-          -- capabilities = {},
-          settings = {
-            Lua = {
-              completion = {
-                callSnippet = "Replace",
-              },
-              -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-              -- diagnostics = { disable = { 'missing-fields' } },
-            },
-          },
+          settings = { Lua = { completion = { callSnippet = "Replace" } } },
         },
       }
 
@@ -882,7 +866,7 @@ require("lazy").setup({
   -- you can continue same window with `<space>sr` which resumes last telescope search
 }, {
   ui = {
-    icons = {}, -- will use nerd font
+    icons = {}, -- empty = will use nerd font
   },
 })
 
