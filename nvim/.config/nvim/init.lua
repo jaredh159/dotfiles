53185f401546847afe835879b95b209a8ccafc92
Jared Henderson <jaredh159@users.noreply.github.com>
2025-07-24 11:46:18 -0400

    nvim: more option tweaks

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -24,30 +24,16 @@ vim.opt.swapfile = false
 vim.g.have_nerd_font = true
 vim.o.mouse = "a"
 vim.o.number = true
-
--- Sync clipboard between OS and Neovim.
---  Schedule the setting after `UiEnter` because it can increase startup-time.
---  Remove this option if you want your OS clipboard to remain independent.
---  See `:help 'clipboard'`
-vim.schedule(function()
-  vim.o.clipboard = "unnamedplus"
-end)
-
--- Enable break indent
-vim.o.breakindent = true
-
--- Save undo history
-vim.o.undofile = true
+vim.o.breakindent = true -- wrapped lines align w/ indent
+vim.o.undofile = true -- save undo history
+vim.o.signcolumn = "yes" -- always show sign col
+vim.o.updatetime = 250 -- Decrease update time
+vim.o.scrolloff = 12
+vim.o.sidescrolloff = 8
 
 -- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
 vim.o.ignorecase = true
 vim.o.smartcase = true
-
--- Keep signcolumn on by default
-vim.o.signcolumn = "yes"
-
--- Decrease update time
-vim.o.updatetime = 250
 
 -- Decrease mapped sequence wait time
 vim.o.timeoutlen = 300
@@ -72,9 +58,6 @@ vim.o.inccommand = "split"
 
 -- Show which line your cursor is on
 vim.o.cursorline = true
-
--- Minimal number of screen lines to keep above and below the cursor.
-vim.o.scrolloff = 10
 
 -- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
 -- instead raise a dialog asking if you wish to save the current file(s)
