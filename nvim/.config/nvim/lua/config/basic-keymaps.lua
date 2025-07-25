34b12e185da95aa2bdb8c343cd290d620c02e8c2
Jared Henderson <jaredh159@users.noreply.github.com>
2025-07-25 14:57:00 -0400

    nvim: reorg keymaps

--- nvim/.config/nvim/lua/config/basic-keymaps.lua
+++ nvim/.config/nvim/lua/config/basic-keymaps.lua
@@ -4,8 +4,12 @@ vim.keymap.set("n", ":", ";")
 vim.keymap.set("v", ";", ":")
 vim.keymap.set("v", ":", ";")
 
+-- split bottom window 33%
 vim.keymap.set("n", "<C-j>", "<cmd>silent !tmux split-window -p 33<CR>", { desc = "Open terminal" })
+-- C-\ to open a vertical split
+vim.keymap.set("n", "<C-\\>", "<cmd>vsplit<CR>", { desc = "Vertical split" })
 
+-- start a session
 vim.keymap.set("n", "<leader>ss", ":Obsession Session.vim<CR>", { desc = "[S]tart [S]ession" })
 
 -- nvim-tree
@@ -13,6 +17,7 @@ vim.keymap.set("n", "<C-b>", ":NvimTreeToggle<CR>", { desc = "Toggle nvim-tree" 
 vim.keymap.set("n", "<leader>sf", ":NvimTreeFindFile<CR>", { desc = "Find file in nvim-tree" })
 
 -- custom save stuff
+vim.keymap.set("n", "<C-s>", ":w<CR>", { desc = "Save current file" })
 vim.keymap.set("n", "<leader>wa", ':silent! wa! | echo "Wrote all writable buffers"<CR>', {
   silent = true,
   desc = "Write all writable",
@@ -21,7 +26,6 @@ vim.keymap.set("n", "<leader>xx", ':silent! wa! | echo "Wrote all writable buffe
   silent = true,
   desc = "Write all writable buffers then quit",
 })
-vim.keymap.set("n", "<C-s>", ":w<CR>", { desc = "Save current file" })
 
 -- copy to system clipboard
 vim.keymap.set("v", "<leader>y", '"+y', { desc = "Copy to system clipboard" })
@@ -44,9 +48,6 @@ vim.keymap.set(
   { desc = "Find files (including hidden)" }
 )
 vim.keymap.set("n", "<leader>ff", "<cmd>Telescope live_grep<CR>", { desc = "Live grep search" })
-
--- C-\ to open a vertical split
-vim.keymap.set("n", "<C-\\>", "<cmd>vsplit<CR>", { desc = "Vertical split" })
 
 -- tmux sessionizer
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer.sh<CR>", { desc = "Open tmux sessionizer" })
