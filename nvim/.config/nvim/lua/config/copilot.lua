3dda78b63831d6fefaad03f61b9d9c0dfabf2a72
Jared Henderson <jaredh159@users.noreply.github.com>
2026-01-03 11:38:59 -0500

    nvim: add copilot toggle keymap

--- nvim/.config/nvim/lua/config/copilot.lua
+++ nvim/.config/nvim/lua/config/copilot.lua
@@ -5,3 +5,16 @@ vim.g.copilot_node_command = "/usr/local/n/versions/node/23.10.0/bin/node"
 vim.g.copilot_filetypes = { ["TelescopePrompt"] = false }
 vim.keymap.set("i", "<C-l>", 'copilot#Accept("\\<CR>")', { silent = true, expr = true, replace_keycodes = false })
 vim.keymap.set("i", "<C-.>", "copilot#Next()", { expr = true })
+
+-- toggle copilot
+vim.keymap.set("n", "<leader>cc", function()
+  if vim.g.copilot_enabled == false then
+    vim.cmd("Copilot enable")
+    vim.g.copilot_enabled = true
+    print("Copilot enabled")
+  else
+    vim.cmd("Copilot disable")
+    vim.g.copilot_enabled = false
+    print("Copilot disabled")
+  end
+end, { desc = "Toggle Copilot" })
